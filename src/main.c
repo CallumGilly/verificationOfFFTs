0c8ca48a99663ddbad2c031f2ce630f46e371a76
callum <callumgilchrist2121@gmail.com>
2025-11-06 12:58:57 +0000

    Remove dead/ obsolete code

--- src/main.c
+++ src/main.c
@@ -3,47 +3,10 @@
 #include <complex.h>
 #include <string.h>
 
-//void function_name(complex float (x_0)[5][6][7], complex float (X)[7][6][5]);
-void fft(complex float (x_0)[2][3][3], complex float (X)[3][3][2]);
+// void fft(complex float (x_0)[2][3][3], complex float (X)[3][3][2]);
 
 
 void main (void) {
-
-  // complex float (*input)[5][6][7] = malloc(sizeof(*input));
-  // memset(input, 0, sizeof(*input));
-  // 
-  // (*input)[1][1][3] = 3 + I * 2;
-  // (*input)[2][6][1] = I * 2;
-  // (*input)[3][1][3] = 3 + I * 2;
-  // (*input)[3][4][3] = I * 2;
-  // (*input)[2][2][7] = I * 2;
-  // (*input)[1][1][3] = I * 2;
-
-  // complex float (*output)[7][6][5] = malloc(sizeof(*output));
-  // memset(output, 0, sizeof(*output));
-  // for (int x_1 = 0; x_1 < 5; x_1++) {
-  //   for (int x_2 = 0; x_2 < 6; x_2++) {
-  //     for (int x_3 = 0; x_3 < 7; x_3++) {
-  //       printf("%.2f+%.2fi, ", creal((*input)[x_1][x_2][x_3]), cimag((*input)[x_1][x_2][x_3]));
-  //       //printf("%.2f+%.2fi", creal(), cimag());
-  //      }
-  //     printf("\n");
-  //   }
-  //   printf("\n");
-  // }
-
-  // printf("\nOutput\n");
-
-  // for (int x_1 = 0; x_1 < 7; x_1++) {
-  //   for (int x_2 = 0; x_2 < 6; x_2++) {
-  //     for (int x_3 = 0; x_3 < 5; x_3++) {
-  //       printf("%.2f+%.2fi, ", creal((*output)[x_1][x_2][x_3]), cimag((*output)[x_1][x_2][x_3]));
-  //       //printf("%.2f+%.2fi", creal(), cimag());
-  //      }
-  //     printf("\n");
-  //   }
-  //   printf("\n");
-  // }
 
   complex float(*input)[2][3][3] = malloc(sizeof(*input));
   memset(input, 0, sizeof(*input));
@@ -75,7 +38,6 @@ void main (void) {
     for (int x_2 = 0; x_2 < 3; x_2++) {
       for (int x_3 = 0; x_3 < 3; x_3++) {
         printf("%.2f+%.2fi, ", creal((*input)[x_1][x_2][x_3]), cimag((*input)[x_1][x_2][x_3]));
-        //printf("%.2f+%.2fi", creal(), cimag());
        }
       printf("\n");
     }
@@ -94,15 +56,4 @@ void main (void) {
     }
     printf("\n");
   }
-
-  // printf("Test of complex multiplication:\n");
-  // complex float randComplexNumber = CMPLXF(0.31, -0.95);
-  // printf("Random Complex number = %.2f+%.2fi\n", creal(randComplexNumber), cimag(randComplexNumber));
-  // complex float minusOne = (I * I);
-  // printf("Minus One = %.2f+%.2fi\n", creal(minusOne), cimag(minusOne));
-  // complex float cplxTimesMinusOne = randComplexNumber * minusOne;
-  // printf("Random Complex number * Minus one = %.2f+%.2fi\n", creal(cplxTimesMinusOne), cimag(cplxTimesMinusOne));
-  
-  //printf("%.2f+%.2fi", creal(n), cimag(n));
-
 }
