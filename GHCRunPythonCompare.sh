484bef84ca64f1c685c12c91a7b851c5db4b5617
callum <callumgilchrist2121@gmail.com>
2025-12-01 12:45:35 +0000

    Small rename

--- GHCRunPythonCompare.sh
+++ GHCRunPythonCompare.sh
@@ -1,3 +1,3 @@
 agda_result="$(agda --compile Implementations/FFT.agda 1> /dev/null && ./FFT)"
 echo "$agda_result"
-nix-shell --command 'python ./FFT.py "'"$(echo "$agda_result" | grep "Flat Tensor:")"'"'
+nix-shell --command 'python ./GHCCompareFFT.py "'"$(echo "$agda_result" | grep "Flat Tensor:")"'"'
